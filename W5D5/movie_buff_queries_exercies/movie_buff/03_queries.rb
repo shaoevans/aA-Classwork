54a1a53737c05c56069ea98868155562a48d8c19
Evans Shao <evanzshao@gmail.com>
2019-11-16 14:38:15 -0800

    polls_app

---
+++ W5D5/movie_buff_queries_exercies/movie_buff/03_queries.rb
@@ -0,0 +1,68 @@
+def what_was_that_one_with(those_actors)
+  # Find the movies starring all `those_actors` (an array of actor names).
+  # Show each movie's title and id.
+  Movie
+    .select(:title, :id)
+    .joins(:actors)
+    .where("actors.name IN (?)", those_actors)
+    .group(:id)
+    .having('COUNT(actors.name) = ?', those_actors.length)
+
+
+end
+
+def golden_age
+  # Find the decade with the highest average movie score.
+  Movie
+    .select('(yr/10)*10 AS decade, AVG(score)' )
+    .group('decade')
+    .order('AVG(score) DESC')
+    .limit(1)
+    .first.decade
+end
+
+def costars(name)
+  # List the names of the actors that the named actor has ever
+  # appeared with.
+  # Hint: use a subquery
+  actor_movies =  Movie.joins(:actors).where('actors.name = ?', name).pluck(:title)              
+  Actor  
+    .joins(:movies)
+    .where('title IN (?)', actor_movies)
+    .where.not('actors.name = ?', name)
+    .distinct.pluck(:name)
+
+  
+end
+
+def actor_out_of_work
+  # Find the number of actors in the database who have not appeared in a movie
+  Actor
+    .left_joins(:movies)
+    .where('movies.title IS NULL')
+    .pluck('COUNT(*)').first
+end
+
+def starring(whazzername)
+  # Find the movies with an actor who had a name like `whazzername`.
+  # A name is like whazzername if the actor's name contains all of the
+  # letters in whazzername, ignoring case, in order.
+
+  # ex. "Sylvester Stallone" is like "sylvester" and "lester stone" but
+  # not like "stallone sylvester" or "zylvester ztallone"
+  matcher = "%#{whazzername.split(//).join('%')}%"
+  Movie.joins(:actors).where('UPPER(actors.name) LIKE UPPER(?)', matcher)
+end
+
+def longest_career
+  # Find the 3 actors who had the longest careers
+  # (the greatest time between first and last movie).
+  # Order by actor names. Show each actor's id, name, and the length of
+  # their career.
+  Actor
+    .select(:name, :id, 'MAX(yr) - MIN(yr) AS career')
+    .joins(:movies)
+    .group(:id)
+    .order('career DESC, name')
+    .limit(3)
+end
---
+++ W5D5/polls_app/app/models/response.rb
@@ -0,0 +1,11 @@
+class Response < ApplicationRecord
+    belongs_to :respondent,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
+
+    belongs_to :answer_choice,
+        primary_key: :id,
+        foreign_key: :answer_choice_id,
+        class_name: :AnswerChoice
+end
